7e3860d55a4d365513f5656c4b9416a2a2db9553
João Valar <joaoperdo@gmail.com>
2021-10-10 18:01:22 -0300

    Showing board of the game

--- Campominado/Campominado.cpp
+++ Campominado/Campominado.cpp
@@ -1,20 +1,45 @@
-// Campominado.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//#include <bits/stdc++.h>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace sf;
+
+
+int main(){
+    srand(time(0));
+    RenderWindow app(VideoMode(400, 400), "Inicia o Campo Minado");
+
+    int w = 32;
+    int grid[12][12];
+    int sgrid[12][12];
+
+    Texture texture;
+    texture.loadFromFile("img/icones.jpg");
+    Sprite s(texture);
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 0; j <= 10; j++) {
+            sgrid[i][j]=10;
+        }
+    }
+
+    while (app.isOpen()) {
+        Event e;
+        while (app.pollEvent(e)) {
+            if (e.type == Event::Closed){
+                app.close();
+            }
+        }
+
+        app.clear(Color::White);
+        for (int i = 1; i <= 10;i++) {
+            for (int j = 1; j <= 10;j++) {
+                s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
+                s.setPosition(i * w, j * w);
+                app.draw(s);
+            }
+        }
+        app.display();
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
